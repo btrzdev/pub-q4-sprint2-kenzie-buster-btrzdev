c712026ff70b02301dcc0038c74959bbf19e6d8e
Beatriz Silva <btrz.dev@gmail.com>
2022-06-25 11:20:59 -0300

    remove localhost reference on message

--- src/app.ts
+++ src/app.ts
@@ -27,5 +27,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`[server]: Server is running at https://localhost:${port}`);
+  console.log(`[server]: Server is running at ${port}`);
 });
