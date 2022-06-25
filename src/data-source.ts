3ef3fb716b3c9424db417651f9254e5bde0dfd0b
Beatriz Silva <btrz.dev@gmail.com>
2022-06-25 01:47:45 -0300

    Fix database connection

--- src/data-source.ts
+++ src/data-source.ts
@@ -4,15 +4,15 @@ import path from "path";
 
 require("dotenv").config();
 
+const databaseUrl: string = process.env.DATABASE_URL;
+
+if (!databaseUrl) {
+  throw new Error("Missing DATABASE_URL env var");
+}
+
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
-  port: 5432,
-
-  username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PWD,
-  database: process.env.POSTGRES_DB,
-
+  url: databaseUrl,
   logging: true,
   entities: [path.join(__dirname, "/entities/**/*.{ts, js}")],
   migrations: [path.join(__dirname, "/migrations/**/*.{ts, js}")],
